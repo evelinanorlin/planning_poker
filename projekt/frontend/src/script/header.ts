130356a54f8123d1e9ef4e110cf8a5c40b7ce577
jenmwa <jenmwa@gmail.com>
2023-05-04 11:44:39 +0200

    html structure added

---
+++ projekt/frontend/src/script/header.ts
@@ -0,0 +1,42 @@
+
+export function renderHeader() {
+    const app = document.querySelector('#app');
+    console.log('connected')
+
+    const header = document.createElement('header');
+    header.setAttribute('class', 'header');
+
+    const title = document.createElement('h1');
+    title.textContent = 'Ivars PlaneringsPoker';
+
+    const button = document.createElement('button');
+
+    if (sessionStorage.getItem('isLoggedIn')) {
+        button.textContent = 'Logga Ut';
+    } else {
+        button.textContent = 'Logga In';
+    }
+
+    button.addEventListener('click', () => {
+        console.log('Button clicked!');
+
+        if (sessionStorage.getItem('isLoggedIn')) {
+            sessionStorage.removeItem('isLoggedIn');
+            button.textContent = 'Logga IN';
+            title.textContent = 'Ivars PlaneringsPoker';
+
+        } else {
+            sessionStorage.setItem('isLoggedIn', 'true');
+            button.textContent = 'Logga Ut';
+            title.textContent = 'Välkommen {{user}}';
+        }
+
+
+      });
+
+    header.appendChild(title);
+    header.appendChild(button);
+
+    app?.appendChild(header);
+}
+
--- projekt/frontend/src/script/main.ts
+++ projekt/frontend/src/script/main.ts
@@ -1,1 +1,12 @@
-import '../styles/style.scss'
+console.log("were connected");
+import '/src/styles/_header.scss';
+// import { io } from 'https://cdn.socket.io/4.3.2/socket.io.esm.min.js';
+// const socket = io("http://localhost:3001");
+
+import { renderHeader } from './header';
+
+const init = () => {
+renderHeader();
+  };
+  
+  init();
