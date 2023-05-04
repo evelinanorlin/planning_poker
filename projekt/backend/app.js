aef612f92e8c98f34a8bf585e8f5e0f1ed4d6b30
jenmwa <jenmwa@gmail.com>
2023-05-04 09:33:04 +0200

    db-connection
    Co-authored-by: Alen Gusinac <alengusinac@users.noreply.github.com>
    Co-authored-by: Evelina <evelinanorlin@users.noreply.github.com>
    Co-authored-by: Sebastian Post <ZynCitrus@users.noreply.github.com>

--- projekt/backend/app.js
+++ projekt/backend/app.js
@@ -1,20 +1,46 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
+const server = require("http").Server(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: process.env.CLIENT_URI,
+    methods: ["GET", "POST"],
+  },
+});
+
+mongoose.connect(process.env.DATABASE_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//Cloud
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
+app.use(cors());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-module.exports = app;
+module.exports = { app: app, server: server };
