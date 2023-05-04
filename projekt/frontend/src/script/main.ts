bf7f5e048ad68d1dd7b4a31560182ad3d74fef04
alengusinac <61206891+alengusinac@users.noreply.github.com>
2023-05-04 11:14:54 +0200

    added checkLogin function

--- projekt/frontend/src/script/main.ts
+++ projekt/frontend/src/script/main.ts
@@ -1,1 +1,20 @@
 import '../styles/style.scss'
+
+import { printLogin } from './login';
+
+export function checkLogin() {
+  if (localStorage.getItem("user")) {
+    printHTML();
+  } else {
+    printLogin();
+  }
+}
+
+function printHTML() {
+  const app = document.querySelector("#app") as HTMLDivElement;
+  if (app != undefined) {
+    app.innerHTML = `<h1>INLOGGAD!!</h1>`;
+  }
+}
+
+checkLogin();
